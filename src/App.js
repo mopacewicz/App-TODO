5e581ad41c481a8a61389a86a567e79d3a75b4a0
Monika Pacewicz <monikapacewicz@MacBook-Pro-Monika.local>
2018-07-08 00:25:42 +0200

    prototype components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+function AppHeader() {
+    return (
+        <header>
+                <h1>List Todo</h1>
+                <div>
+                    <button>Add Todo</button>
+                </div>
+        </header>
+    );
+}
+
+function TodoFilter() {
+    return (
+        <div>
+            <button>Show all</button>
+            <button>Completed</button>
+            <button>Incompleted</button>
+        </div>
+    );
+}
+
+function TodoList() {
+    return (
+        <ul>
+            <TodoItem
+                name="Orange"
+                category="Shopping"
+            />
+            <TodoItem
+                name="Kitchen"
+                category="Cleaning"
+            />
+            <TodoItem
+                name="Bike"
+                category="Sport"
+            />
+        </ul>
+    );
+}
+
+function TodoItem({ name, category }) {
+    return (
+        <li>
+            <div>
+                <input type="checkbox"></input>
+                <h4>{name}</h4>
+                <div>{category}</div>
+                <button>Edit</button>
+                <button>Remove</button>
+            </div>
+        </li>
+    );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <AppHeader />
+            <TodoFilter/>
+            <TodoList/>
+        </div>
     );
   }
 }
